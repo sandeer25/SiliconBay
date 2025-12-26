a9c9e91626186a9ba6f46b0853e2aa031ebe4790
sanethdassanayake <sanethdassanayake2005@gmail.com>
2025-12-26 13:09:17 +0530

    Added Account page

---
+++ app/account/page.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const navigationList = [
+  { name: "Overview", href: "/account/overview" },
+  { name: "Orders", href: "/account/orders" },
+  { name: "Payments", href: "/account/payments" },
+  { name: "Returns", href: "/account/returns" },
+  { name: "Wishlist", href: "/account/wishlist" },
+  { name: "Feedback", href: "/account/feedback" },
+  { name: "Addresses", href: "/account/addresses" },
+  { name: "Settings", href: "/account/settings" },
+];
+
+const Account = () => {
+  return (
+    <div className="container mx-auto border flex items-center justify-center">
+      <div className="flex flex-col items-center text-center space-y-2">
+        <h1 className="text-xl font-bold">Navigation Links</h1>
+        {navigationList.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            className="underline text-blue-600"
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Account;
